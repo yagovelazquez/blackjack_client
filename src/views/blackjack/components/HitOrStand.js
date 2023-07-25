59896b8d21264dfe82bc664e2d0eff2f3e397a94
Yago Velazquez <yago@coolset.com>
2023-07-25 08:03:10 +0100

    feat: add points to game

--- src/views/blackjack/components/HitOrStand.js
+++ src/views/blackjack/components/HitOrStand.js
@@ -6,6 +6,19 @@ import GameCards from './GameCards';
 import { Fragment, useMemo } from 'react';
 import Headings from '../../../shared/components/texts/Heading';
 
+function Points({ points, participant }) {
+  const containerClass =
+    participant === 'Dealer' ? 'top-10 left-10' : 'bottom-10 left-10';
+  return (
+    <div className={`absolute flex items-center flex-col ${containerClass}`}>
+      <span className="text-[30px]">{participant}</span>
+      <div className="bg-black rounded-full w-12 h-12 text-2xl flex items-center justify-center">
+        {points}
+      </div>
+    </div>
+  );
+}
+
 function HitOrStand({ onHit, onStand, handData, onPlayAgain }) {
   const winner = useMemo(() => {
     if (handData.winner === 'draw') {
@@ -15,6 +28,8 @@ function HitOrStand({ onHit, onStand, handData, onPlayAgain }) {
   }, [handData.winner]);
   return (
     <div className="text-white relative flex h-full w-full justify-between items-center">
+      <Points points={handData.player.points} participant="Player" />
+      <Points points={handData.dealer.points} participant="Dealer" />
       <Fragment>
         {handData.winner ? (
           <div className="w-full flex-col gap-2 justify-center items-center flex">
